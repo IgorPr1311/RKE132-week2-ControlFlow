f09de6bccbebc853a06292d2cb15522e142d6066
Igor Pr <igor.prokofjev@tktk.ee>
2023-02-24 15:09:31 +0200

    add week2 code

---
+++ UserAgeValidation/Program.cs
@@ -0,0 +1,51 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+//rakendus küsib kasutaja vanust
+//kui vanus on alla 13, siis konsoolis kuvatakse teade:
+//"You are too young to use Instagram"
+//muul juhul konsoolis kuvatakse
+//"Welcome to Instagram
+
+using System.ComponentModel.Design;
+
+Console.WriteLine("Enter your age:");
+
+string userAge = Console.ReadLine();
+
+int userAgeNum = 0;
+
+bool isAgeNumber = Int32.TryParse(userAge, out userAgeNum);
+
+Console.WriteLine($"Parse result {isAgeNumber}. User is {userAgeNum} years old.");
+
+if (isAgeNumber)
+{
+    if (userAgeNum >= 13) 
+    {
+        Console.WriteLine("Welcome to Instagram!");
+    }
+    else 
+    {
+        Console.WriteLine("You are too young!");
+    }
+}
+else
+{
+    Console.WriteLine("Could not read your age");
+}
+
+
+
+
+
+
+
+//int userAge = Int32.Parse(Console.ReadLine());//"13"- "sõne" heap(hunnik), nõuab rohkem aega ja arvuti resurssi, et info üles leida. 13 - läheb stack-i
+//if(userAge >= 13)
+{
+    //Console.WriteLine("Welcome to Instagram!");
+}    
+//else
+{
+    //Console.WriteLine("You are too young to use the Instagram!");
+}    
---
+++ UserGenderValidation/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+//rakendus palub kasutajat valida tema sugu (m/f)
+//rakendus küsib kasutaja perekonnanime
+//lähtudes valitud soost, rakendus tervitab kasutajat järgmiselt
+//"Welcome, Mr.[kasutaja perekonnanimi]" või "Welcome, Ms. [kasutaja perekonnanimi]"
+
+Console.WriteLine("Please, select your gender (m/f):");
+
+char userGender = Char.Parse(Console.ReadLine());//loeb konsoolis maha andmed string e. sõne formaadis
+Console.WriteLine("Please, enter your last name:");
+string userLastName = Console.ReadLine();
+if (userGender == 'm')
+{
+    Console.WriteLine($"Welcome, Mr. {userLastName}!");
+}
+else if (userGender == 'f')
+{
+    Console.WriteLine($"Welcome, Ms. {userLastName} !");
+}
+else
+{
+     Console.WriteLine($"Welcome, {userLastName}!");
+}
